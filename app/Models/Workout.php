e7e3de8d9cdf327c3dc05dbfaa9ea5e8c4b21a68
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-28 15:03:51 +0000

    updated Model with $fillable that was set up on the migration. exercise, sets, reps, weight, category

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Workout extends Model
 {
-    //
+    // set fillable that was set up on the migration
+    protected $fillable = [
+        'exercise',
+        'set',
+        'reps',
+        'weight',
+        'category',
+    ];
 }
