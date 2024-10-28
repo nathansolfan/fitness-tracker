226feb0439be3dc574d4830850df0d2ad5f986a3
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-28 15:17:39 +0000

    added the 2 routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\WorkoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [WorkoutController::class, 'index'])->name('workouts.index');
+Route::resource('workouts', WorkoutController::class);
