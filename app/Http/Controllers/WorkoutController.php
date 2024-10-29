7a4840d28cf659405915b2d712143c36a47ef049
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-29 15:32:21 +0000

    update controller with destroy

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -78,8 +78,9 @@ class WorkoutController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Workout $workout)
     {
-        //
+        $workout->delete();
+        return redirect()->route('workouts.index');
     }
 }
