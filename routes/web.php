6b68839620691806cb8af8609fcbfbba9ac0ec14
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-30 16:25:23 +0000

    imported import Chart from 'chart.js/auto'; Into app.js
    and created the analytics route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 
 Route::get('/', [WorkoutController::class, 'index'])->name('workouts.index');
 Route::resource('workouts', WorkoutController::class);
+
+
+// ANALYTICS Route
+Route::get('/workouts/analytics', [WorkoutController::class, 'analytics'])->name('workouts.analytics');
