313dc020dbe5d235ca1d4ee1725a157444d1d262
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-30 23:53:21 +0000

    create factory and added the definitions

---
+++ database/factories/WorkoutFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Workout>
+ */
+class WorkoutFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'exercise' => $this->faker->word,
+            'sets' => $this->faker->numberBetween(1,5),
+            'reps' => $this->faker->numberBetween(5,15),
+            'weight' => $this->faker->optional()->numberBetween(10,150),
+            'category' => $this->faker->randomElement(['strength', 'cardio']),
+        ];
+    }
+}
