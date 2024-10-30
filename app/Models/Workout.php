b3d8ede943df4c7654b3aaed9baf3ac96d29ef4b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-30 23:56:29 +0000

    updated with seeder

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Workout extends Model
 {
+    use HasFactory;
+
     // set fillable that was set up on the migration
     protected $fillable = [
         'exercise',
---
+++ database/seeders/WorkoutSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Workout;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WorkoutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Define the Workout Seeder
+        Workout::factory()->count(20)->create();
+    }
+}
