ea7bfb56dd8e9a04c214d59230828d871fbf1e5c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-01 15:53:06 +0000

    grouped each exercise

--- resources/views/workouts/index.blade.php
+++ resources/views/workouts/index.blade.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Workouts</title>
-</head>
-<body>
-    <h1>Workouts</h1>
-    <a href="{{ route('workouts.create') }}">Add New Workout</a>
-    <ul>
-        @foreach($workouts as $workout)
-            <li>
-                {{ $workout->exercise }} - {{ $workout->category }}
-                <a href=" {{ route('workouts.show', $workout)}} ">View</a>
-                <a href="{{ route('workouts.edit', $workout) }}">Edit</a>
-                <a href="{{ route('workouts.progress', ['exercise' => $workout->exercise]) }}">View Progress</a>
-
-                <form action="{{ route('workouts.destroy', $workout) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
-</body>
-</html>
+<ul>
+    @foreach($workouts->groupBy('exercise') as $exercise => $workoutGroup)
+        <li>
+            <strong>{{ $exercise }}</strong> - {{ $workoutGroup->first()->category }}
+            <br>
+            Total Sessions: {{ $workoutGroup->count() }}
+            <br>
+            Latest Weight: {{ $workoutGroup->last()->weight }} kg
+            <br>
+            <a href="{{ route('workouts.progress', ['exercise' => $exercise]) }}">View Progress</a>
+            <form action="{{ route('workouts.destroy', $workoutGroup->first()) }}" method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete All Sessions</button>
+            </form>
+        </li>
+        <hr>
+    @endforeach
+</ul>
